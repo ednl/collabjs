1a91451134436350065c9b7a11c3d3dbad22e21e
Ewoud Dronkert <ewoud@dronkert.net>
2020-07-25 13:58:45 +0200

    Working version

--- public/sketch.js
+++ public/sketch.js
@@ -1,4 +1,4 @@
-let fg, pencil, clearall;
+let col, pencil, clear, save, socket;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -8,35 +8,55 @@ function setup() {
 	const r = floor(random(256));
 	const g = floor(random(256));
 	const b = floor(random(256));
-	fg = createColorPicker(color(r, g, b));
-	fg.position(0, 0);
-
+	col = createColorPicker(color(r, g, b));
 	pencil = createSlider(1, 100, 10, 1);
 	pencil.style('width', '200px');
+	clear = createButton('Wissen');
+	clear.mousePressed(() => background(255));
+	save = createButton('Bewaren');
+	save.mousePressed(() => saveCanvas('tekening', 'jpg'));
+	posUI();
+
+	socket = io.connect('http://localhost:3000');
+	socket.on('mouse', data => plot(data));
+}
+
+function posUI() {
+	col.position(0, 0);
 	pencil.position((width - pencil.width) / 2, 0);
-
-
-	clearall = createButton('Clear All');
-	clearall.position(width - clearall.width, 0);
-	clearall.mousePressed(() => background(255));
+	clear.position(width - clear.width, 0);
+	save.position(width - clear.width - save.width - 5, 0);
 }
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
-	pencil.position((width - pencil.width) / 2, 0);
-	clearall.position(width - clearall.width, 0);
+	background(255);
+	posUI();
+}
+
+function plot(data) {
+	fill(data.r, data.g, data.b);
+	const scalex = width / data.w;
+	const scaley = height / data.h;
+	circle(data.x * scalex, data.y * scaley, data.s * scalex);
 }
 
 function mouseDragged() {
+	const x = mouseX;
 	const y = mouseY;
-	if (y >= 20) {
-		const x = mouseX;
-		const s = pencil.value();
-		const c = fg.color();
-		fill(c);
-		circle(mouseX, mouseY, s);
-
-		const data = '{x:' + x + ',y:' + y + ',s:' + s + ',r:' + red(c) + ',g:' + green(c) + ',b:' + blue(c) + '}';
-		console.log(data);
+	if (x >= 0 && x < width && y >= 20 && y < width) {
+		const c = col.color();
+		const data = {
+			x: x,
+			y: y,
+			s: pencil.value(),
+			r: red(c),
+			g: green(c),
+			b: blue(c),
+			w: width,
+			h: height
+		};
+		plot(data);
+		socket.emit('mouse', data);
 	}
 }
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-// TODO
+const express = require('express');
+const app = express();
+const server = app.listen(3000, listen);
+
+function listen() {
+	const host = server.address().address;
+	const port = server.address().port;
+	console.log('Server listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
+
+const io = require('socket.io')(server);
+io.sockets.on('connection', socket => {
+	console.log("New client: " + socket.id);
+	socket.on('mouse', data => socket.broadcast.emit('mouse', data));
+	// socket.on('disconnect', () => console.log("Client has disconnected"));
+});
